a54b54d39acf10b72af1453727f539b75d23c6ae
jerrinot <jaromir.hamala@gmail.com>
2020-01-23 13:11:14 +0100

    wip

--- jetsource/src/test/java/info/jerrinot/hazelcastbridge/jetsource/Server.java
+++
@@ -1,9 +0,0 @@
-package info.jerrinot.hazelcastbridge.jetsource;
-
-import com.hazelcast.core.Hazelcast;
-
-public class Server {
-    public static void main(String[] args) {
-        Hazelcast.newHazelcastInstance();
-    }
-}
